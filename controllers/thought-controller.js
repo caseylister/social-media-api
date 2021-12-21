76d20e88a21bdc21a0dbb9624098f51da089fb88
caseylister <caseynlister@gmail.com>
2021-12-21 18:21:48 -0500

    create / update thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,6 +6,7 @@ const thoughtController = {
         Thought.find({})
         .populate({ path: 'user', select: '-__v'})
         .select('-__v')
+        .sort({ _id: -1 })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
@@ -24,5 +25,44 @@ const thoughtController = {
             res.status(500).json(err);
         })
     },
-    
+
+    // createThought (POST)
+    createThought({ body }, res){
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { username: body.username },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+            .then(dbUserData => {
+                if(!dbUserData){
+                    res.status(404).json({ message: 'No user found with this username!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // updateThought by id (PUT)
+    updateThought({ params, body}, res){
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        )
+        .then(updatedThought => {
+            if(!updatedThought){
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(updatedThought);
+        })
+        .catch(err => res.json(err));
+
+    }
+
+
 }
