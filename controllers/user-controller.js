3982d0635189c3e0c551a83a527fa1758e3aafc2
caseylister <caseynlister@gmail.com>
2021-12-21 18:47:55 -0500

    get all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+
+const userController = {
+    // GET all users
+    getUsers(req, res){
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
+}
