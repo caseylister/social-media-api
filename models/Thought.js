71594f5a1b67a15e0e0b98eeb34fb94f05ce5f01
caseylister <caseynlister@gmail.com>
2021-12-21 15:51:30 -0500

    thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -28,3 +28,29 @@ const ReactionSchema = new Schema({
     }
 );
 
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: 'User'
+    },
+    reactions: [ReactionSchema],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
