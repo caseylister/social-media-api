1b9b66b6b9492bac0fb100b9e203e66163008a4a
caseylister <caseynlister@gmail.com>
2021-12-21 19:31:12 -0500

    create, update, delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,34 +1,74 @@
-const { User } = require('../models');
+const { User, Thought } = require("../models");
 
 const userController = {
-    // GET all users
-    getUsers(req, res){
-        User.find({})
-        .populate({
-            path: 'thoughts',
-            select: '-__v'
-        })
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
-    },
+  // GET all users
+  getUsers(req, res) {
+    User.find({})
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
-    // GET users by id /api/users/:id
-    getUserById({ params}, res){
-        User.findOne({ _id: params.id })
-        .populate({
-            path: 'thoughts',
-            select: '-__v'
-        })
-        .populate({
-            path: 'friends',
-            select: '-__v'
-        })
-        .select('-__v')
+  // GET users by id /api/users/:id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // create user (POST)
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update user (PUT)
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate(
+    { _id: params.id },
+    body,
+    { new: true, runValidators: true })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // DELETE /api/users/:id
+  removeUser({ params}, res){
+      Thought.deleteMany({ userId: params.id })
+      .then(() => {
+        User.findOneAndDelete({ _id: params.id })
         .then(dbUserData => {
             if(!dbUserData){
                 res.status(404).json({ message: 'No user found with this id!' });
@@ -36,13 +76,10 @@ const userController = {
             }
             res.json(dbUserData);
         })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    },
-
-    
-}
+      })
+      .catch(err => res.json(err));
+  },
+  
+};
 
 module.exports = userController;
