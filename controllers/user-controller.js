464c65d2ffbd733641ae9eb7d296b3743df14391
caseylister <caseynlister@gmail.com>
2021-12-21 19:47:44 -0500

    add / remove friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -79,7 +79,40 @@ const userController = {
       })
       .catch(err => res.json(err));
   },
-  
+
+  // add friend (POST)
+  addFriend({ params }, res){
+      User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { friends: params.friendId } },
+          { new: true, runValidators: true }
+      )
+      .then(dbUserData => {
+          if(!dbUserData){
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+          }
+          res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // remove friend (DELETE)
+  removeFriend({ params }, res){
+      User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { friends: params.friendId } },
+          { new: true }
+      )
+      .then((dbUserData => {
+          if(!dbUserData){
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+          }
+          res.json(dbUserData);
+      }))
+      .catch(err => res.json(err));
+  }
 };
 
 module.exports = userController;
